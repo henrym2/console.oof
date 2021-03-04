23e03d3579ab9b2cdff447fce13499d83cb0c602
henrym2 <henrym2@tcd.ie>
2021-03-04 16:41:48 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,32 @@
+# Console.oof
+
+## Installation and usage
+
+Ever felt like your console.log messages weren't fun enough? \\
+Your code not bullying you enough? \\
+Want something more interesting than "BROKEN HERE" in your log messages? \\
+Deathly afraid of the debugger?
+
+Fear not, for this is the package for you! Simply require the module and let the Oof's flow free. 
+
+```bash
+npm i console.oof
+```
+
+```javascript
+const oof = require("console.oof")
+oof()
+```
+It's as simple as that!
+
+## Contribution
+
+Please feel free to contribute you're own prime "Oofs" to the package. All styling is randomized and examples can be found both in the index.js folder and below!. This package is free to be forked, changed and modified but completely at their own risk as per the GLWT (Good Luck With That) Public License.
+
+### Phrase examples
+
+Phrases should be formatted in the following way:
+```javascript
+"%c YOUR TEXT HERE %c SOME MORE TEXT \n %c SOME MORE TEXT"
+```
+Any text following a \%c will have random styling applied to it, so you can either style everything the same or randomize the styling across the text! I might add more styling options later or configuration, but for now this serves its purpose.  
---
+++ index.js
@@ -0,0 +1,53 @@
+const colors = [
+    "red", "blue", "green", "purple", "pink", "orange", "powderBlue", "grey", "black", "white", ""
+]
+
+const borders = [
+    "none", "hidden", "outset", "dashed", "dotted", "groove"
+]
+
+const phrases = [
+    "%c Roses are red\n%c Violets are blue\n%c This code is broken\n%c Fuck you",
+    "%c Welp, that didn't work did it?",
+    "%c Maybe next time buddy",
+    "%c Javascript really has you in a headlock doesn't it?",
+    "%c I hear git reset --hard is handy",
+    "%c rm -rf / \n %c Erase all records of this",
+    "%c Are you sure you're not an engineer?",
+    "%c Ah yes, a stack overflow copy and paste job",
+    "%c Wrote this last week and don't remember what it does anymore?\n%c Too bad",
+    `%c/*
+    ${String.fromCodePoint(0x1F525)} WARNING!!
+    You broke it.
+    */
+    `
+]
+
+const seed = () => Math.floor(Math.random() * 100)
+
+function genStyle(phrase) {
+    const styleCount = phrase.split("\%c").length-1
+    console.log(styleCount)
+    let styles = []
+    for (let i = 0; i < styleCount; i++) {
+        styles.push(
+            [
+                `color: ${colors[seed() % colors.length]}`,
+                'mix-blend-mode: difference',
+                `padding: ${seed()/70}rem`,
+                `font-size: ${(seed()/30) + 1.2}rem`,
+                //`border: ${borders[seed() % borders.length]}`
+            ].join(';')
+        )
+    }
+    return styles
+}
+
+function oof() {
+    const phrase = phrases[seed() % phrases.length]
+    const style = genStyle(phrase)
+
+    return console.log(phrase, ...style)
+}
+
+module.exports = oof
